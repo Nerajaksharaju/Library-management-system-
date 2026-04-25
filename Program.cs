54c2e13bf53f9b3d71b5768a4fd05b3c7af53a29
Nerajaksharaju <nerajaksha27rajuna@gmail.com>
2026-04-25 18:51:38 +0530

    Constructors implementation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
-﻿using LibraryManagementSystem;
+﻿// using LibraryManagementSystem;
 
-UserInterface userInterface = new UserInterface();
-userInterface.MainMenu();
+// UserInterface userInterface = new UserInterface();
+// userInterface.MainMenu();
+
+// (string, int) book1 = ("Frankenstein", 350);
+// Console.WriteLine($"Title: {book1.Item1}, Pages: {book1.Item2}");
+
+// (string Title, int Pages) book2 = ("Frankenstein", 350);
+// Console.WriteLine($"Title: {book2.Title}, Pages: {book2.Pages}");
+
+using TCSA.OOP.LibraryManagementSystem;
+
+var book1 = new Book();
+var book2 = new Book("Great Gatsby", 300);
+var book3 = new Book(280);
